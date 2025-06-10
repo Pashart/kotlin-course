ab8e10b1eb000f8beac078be449bde3c20ebe083
Pashart <pashart2013@gmail.com>
2025-06-10 20:40:02 +0300

    ClassWork5 Part2

--- src/main/kotlin/com/apashart/lesson5/ClassWork5.kt
+++ src/main/kotlin/com/apashart/lesson5/ClassWork5.kt
@@ -54,16 +54,81 @@ fun main() {
     var example7 = ((2 * 5) == 10) && !(7 - 3 < 5) || (8 / 2 == 4) //true
     var example8 = ((9 + 2) < 12) && (15 % 4 != 3) || !(4 * 2 == 8) //false
 
-//    var example9 = ((var a = 4; a-- == 4) || (5 * 2 != 10)) && (3 + 1 == 4)
-//    var example10 = (var b = 3; b++ > 3) && (6 / 2 == 3) || (7 - 2 != 5)
-//    var example11 = (var c = 1; ++c < 3) || ((4 % 2 == 0) && (5 + 0 == 5))
-//    var example12 = ((var d = 5; d-- == 5) && (8 / 4 == 2)) || (2 * 3 != 6)
-//    var example13 = (var e = 2; e++ < 3) && ((7 + 1 == 8) || (4 % 2 != 0))
+//((var a = 4; a-- == 4) || (5 * 2 != 10)) && (3 + 1 == 4)
+//true                   || false          && true      = true +
+//(var b = 3; b++ > 3) && (6 / 2 == 3) || (7 - 2 != 5)
+//false                && true         || false        = false +
+//(var c = 1; ++c < 3) || ((4 % 2 == 0) && (5 + 0 == 5))
+//true                 || true          && true         = true +
+//((var d = 5; d-- == 5) && (8 / 4 == 2)) || (2 * 3 != 6)
+//true                   && true          || false      = true +
+//(var e = 2; e++ < 3) && ((7 + 1 == 8) || (4 % 2 != 0))
+//true                 && true          || false        = true +
 
-    println(example8)
+//Оператор ЕЛВИСА
+
+    val defoltName = "Unknown"
+    var name: String? = null
+
+//В Java если переменная == null то возникает ошибка NullPointerException не возможно вызвать переменную.
+//Это можно решить таким кодом:
+
+/*
+    val resJava = if (name == null) {
+        defoltName
+    } else {
+        name
+    }
+
+//В Котлине есть замена этому коду, оператор ?: - Оператор Елвиса
+
+    val result = name ?: defoltName
+    val result2 = name ?: throw IllegalArgumentException("Не может быть null") //остановка программы + месседж
+    val result3 = name ?: throw Exception("Не может быть null") //остановка программы + месседж
+
+//Null - возникает тогда когда объект создать не удалось по какой-то причине, и программа должна остановится.
+// Чтобы программа не прервалась, мы ставим заглушку null и мы ссылаемся на "ничто"
+//Если нам нужно подставить что-то вместо null, мы с помощью оператора Элвиса можем присвоить дефолтное значение или сделать исключение
+ */
+    printVolume(null)
+    printPrice(123.4, null)
+    printPrice(100.0,13)
+    printLanguage("Русский")
+    printLanguage(null)
+    printInsideBox("Жопа")
+    printInsideBox(null)
+}
+//Задача выводить пользовательский уровень громкости
+
+fun printVolume (userVolume: Int?) {
+    val defaultVolume = 30
+    println(userVolume ?: defaultVolume)
 }
 
+//ПРАКТИЧЕСКАЯ РАБОТА
 
+//Рассчитать стоимость товара со скидкой, если цена может быть изваестна, а скидка не установлена
+// скидка может быть а может не быть, если скидки нет то она может быть null
 
+fun printPrice (price : Double, discount: Int?) {
+    var kof = (100 - (discount ?: 0))/100.0
+    println(price * kof)
+}
 
+//Вернуть язык интерфейса для сайта. Если пользователь не установил язык по умолчанию, то вернуть английский
 
+fun printLanguage (language: String?) {
+//    if (language == null) {
+//        println("English")
+//    } else {
+//        println(language)
+//    }
+    val defoltLanguage = "English"
+    var result1 = language ?: defoltLanguage
+    println(result1)
+}
+//Вы работаете на складе и отвечаете за инвентаризацию и если в каком-то контейнере нет товара, то нужно остановить работу программы
+
+fun printInsideBox (staff: String?) {
+    println(staff ?: throw Exception ("Box is empty"))
+}
